3e4cfa9ed57939213dbd92287cbf186a1623f470
leviking <lev@schoolstatus.com>
2024-12-08 00:50:23 -0600

    add a different way than add-apt-repository

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,5 +1,7 @@
 # Display system information in the terminal
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+sudo sh -c 'echo "deb http://ppa.launchpad.net/zhangsongcui3371/fastfetch/ubuntu focal main" > /etc/apt/sources.list.d/fastfetch.list'
+curl -fsSL https://keyserver.ubuntu.com/pks/lookup?op=get&search=0xF566E19D8027B3EB1A3FC47E42E5C84D | gpg --dearmor | sudo tee /etc/apt/keyrings/fastfetch.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/fastfetch.gpg] https://ppa.launchpadcontent.net/zhangsongcui3371/fastfetch/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/fastfetch.list
 sudo apt update -y
 sudo apt install -y fastfetch
 
