d7a15d49f77ee8174beb2229eb35533e36742658
leviking <lev@schoolstatus.com>
2024-12-08 00:31:57 -0600

    feat: add arch suffix

--- install/terminal/required/app-gum.sh
+++ install/terminal/required/app-gum.sh
@@ -1,7 +1,19 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "armv6l" ]]; then
+  echo "Running on ARMv6 architecture"
+  ARCH_SUFFIX="armhf6"
+elif [[ "$ARCH" == "armv7l" ]]; then
+  echo "Running on ARMv7 architecture"
+else
+  echo "Architecture not recognized as ARMv6 or ARMv7"
+  exit 1
+fi
+
 cd /tmp
 GUM_VERSION="0.14.3" # Use known good version
-wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_armhf6.deb"
+wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_${ARCH_SUFFIX}.deb"
 sudo apt-get install -y ./gum.deb
 rm gum.deb
 cd -
