ffc8ff093d64d254e021771e284e05180fb27aa7
leviking <lev@schoolstatus.com>
2024-12-08 03:05:50 -0600

    make fastfetch executable

--- install/terminal/app-fastfetch.sh
+++ install/terminal/app-fastfetch.sh
@@ -1,6 +1,7 @@
 # Display system information in the terminal
 echo "extracting fastfetch"
-tar -xzf ~/.local/share/omakub/tars/fastfetch.tar.gz ~/.local/share/omakub/bin/ 
+tar -xzf ~/.local/share/omakub/tars/fastfetch.tar.gz -C ~/.local/share/omakub/bin/
+chmod +x ~/.local/share/omakub/bin/fastfetch
 
 # Only attempt to set configuration if fastfetch is not already set
 if [ ! -f "$HOME/.config/fastfetch/config.jsonc" ]; then
