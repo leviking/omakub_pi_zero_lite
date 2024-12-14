d119d18c9a62ed607d7787211067991dc4db6504
leviking <lev@schoolstatus.com>
2024-12-14 00:42:23 -0600

    feat: add mise
    for armv6 at least mise release has been removed, so we need to use this
    specific version.

---
+++ install/terminal/mise.sh
@@ -0,0 +1,3 @@
+curl -L https://github.com/jdx/mise/releases/download/v2024.5.23/mise-v2024.5.23-linux-armv6 > mise-v2024.5.23
+sudo mv mise-v2024.5.23 /usr/local/bin/mise
+sudo chmod +x /usr/local/bin/mise
